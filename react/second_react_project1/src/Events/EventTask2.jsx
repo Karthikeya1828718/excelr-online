ecc4522de171f492a1f77a54abf963987541c804
Karthikeya1828718 <kasikarthikeya587@gmail.com>
2025-05-05 21:41:35 +0530

    removed one file from react

--- react/second_react_project1/src/Events/EventTask2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const EventTask2 = () => {
-  return (
-    <div>EventTask2</div>
-  )
-}
-
-export default EventTask2
--- react/second_react_project1/src/context/PropertyContext.jsx
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-//createContext is  a function used to create context object
-export const PropertyContext=createContext(null);
