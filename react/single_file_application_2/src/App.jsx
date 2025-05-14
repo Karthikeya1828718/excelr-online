7d945dbc700ec1dad75abac284cdd47ffcfe9dfa
Karthikeya1828718 <kasikarthikeya587@gmail.com>
2025-05-14 12:39:16 +0530

    day 34

--- react/single_file_application_2/src/App.jsx
+++ react/single_file_application_2/src/App.jsx
@@ -3,6 +3,11 @@ import FetchProducts from "./components/FetchProducts";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import FetchUsers from "./components/FetchUsers";
+import SearchInput from "./components/SearchInput";
+import DeleteProduct from "./components/DeleteProduct";
+import UpdateProduct from "./components/UpdateProduct";
+import EditProduct from "./components/EditProduct";
+import SaveProduct from "./components/SaveProduct";
 function App() {
   return (
     <>
@@ -14,8 +19,19 @@ function App() {
       
 
         <Routes>
-        <Route path="/products" element={<FetchProducts />}></Route>
-        <Route path="/users" element={<FetchUsers/>}></Route>
+          <Route path="/saveProduct" element={<SaveProduct />}></Route>
+          <Route path="/products" element={<FetchProducts />}></Route>
+          <Route path="/users" element={<FetchUsers />}></Route>
+          <Route path="/deleteProduct" element={<DeleteProduct />}></Route>
+          <Route path="/updateProduct" element={<UpdateProduct />}></Route>
+          <Route path="/editProduct/:id" element={<EditProduct />}></Route>
+           <Route path="/search/:name" element={<SearchInput />}></Route>
+          {/* try to fetch list of users */}
+
+          
+          {/* <Route path="/deleteUser/:id" element={<DeleteUser />}></Route>
+          <Route path="/updateUser/:id" element={<UpdateUser />}></Route> */}
+         
         </Routes>
       </BrowserRouter>
     </>
